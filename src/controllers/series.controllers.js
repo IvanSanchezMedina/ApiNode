262b4ab8c9049d3286925edf0754707c5f671b71
Iván Ulises Sánchez Medina <ivanusanchezm@gmail.com>
2024-03-04 18:04:54 -0600

    Update Get Series

--- src/controllers/series.controllers.js
+++ src/controllers/series.controllers.js
@@ -3,9 +3,21 @@ import { pool } from '../db.js';
 export const getSeries = async(req, res) => {
     try{
 
-        const [rows] = await pool.query('Select  * From series order by id asc limit 3');
+        const [rows] = await pool.query(`
+            SELECT series.*, users.first_name, users.last_name
+            FROM series
+            INNER JOIN users ON series.user_id = users.id
+            WHERE series.status != 'Deleted'
+            ORDER BY series.id DESC;
+        `);
 
-        res.json(rows);
+        const array = {
+            status : 'success',
+            message: "List for series",
+            items : rows
+        }
+
+        res.json(array);
     } catch(error){
         return res.status(500).json({
             message:'No se puede consultar la serie'
--- src/routes/series.routes.js
+++ src/routes/series.routes.js
@@ -5,7 +5,7 @@ import { getSeries, getSerie } from "../controllers/series.controllers.js";
 const router = Router()
 
 router.get( '/series',verifyToken, getSeries)
-router.get('/series/:id',verifyToken,getSerie)
+router.get('/serie/:id',verifyToken,getSerie)
 
 export default router
 
